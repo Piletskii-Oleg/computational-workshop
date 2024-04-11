79a915d421a60399d168d2ac0e8cad872ed8cce5
Piletskii-Oleg <piletskii.oleg@gmail.com>
2024-04-11 22:48:01 +0300

    add tridiagonal matrix to examples, negative variation

--- condition_numbers/src/examples.rs
+++ condition_numbers/src/examples.rs
@@ -3,8 +3,12 @@ use ndarray::{array, Array1, Array2};
 pub struct Examples;
 
 impl Examples {
-    pub fn vector() -> Array1<f64> {
+    pub fn vector2() -> Array1<f64> {
         array![200.0, -600.0]
+    }
+
+    pub fn vector_n(n: usize) -> Array1<f64> {
+        (0..n).map(|i| (i * i) as f64).collect()
     }
 
     pub fn vector_changed() -> Array1<f64> {
@@ -20,6 +24,29 @@ impl Examples {
             .map(|row| {
                 (0..n)
                     .map(|column| 1.0 / (row + column + 1) as f64)
+                    .collect()
+            })
+            .collect::<Vec<Vec<f64>>>();
+
+        let dimensions = (n, n);
+        Array2::from_shape_vec(
+            dimensions,
+            matrix.into_iter().flatten().collect::<Vec<f64>>(),
+        )
+        .unwrap()
+    }
+
+    pub fn tridiagonal(n: usize) -> Array2<f64> {
+        let matrix = (0..n)
+            .map(|row| {
+                (0..n)
+                    .map(|column| {
+                        if row.abs_diff(column) == 1 {
+                            (row * column + 1) as f64
+                        } else {
+                            0.0
+                        }
+                    })
                     .collect()
             })
             .collect::<Vec<Vec<f64>>>();
--- condition_numbers/src/main.rs
+++ condition_numbers/src/main.rs
@@ -5,16 +5,22 @@ use ndarray_linalg::Solve;
 use prettytable::{row, Cell, Row, Table};
 
 const VARIATIONS: [f64; 7] = [1.0, 0.1, 1e-2, 1e-4, 1e-6, 1e-8, 1e-10];
+const NEGATIVE_VARIATIONS: [f64; 7] = [-1.0, -0.1, -1e-2, -1e-4, -1e-6, -1e-8, -1e-10];
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let bad_matrix = Examples::bad_matrix();
-    let vector = Examples::vector();
+    let vector = Examples::vector2();
 
     let hilbert = Examples::hilbert(3);
-    let hilbert_vec = array![3.0, 2.0, -4.0];
+    let hilbert_vec = array![1.0, 2.0, 1.0];
+
+    const TRIDIAGONAL_SIZE: usize = 4;
+    let tridiagonal = Examples::tridiagonal(TRIDIAGONAL_SIZE);
+    let vector_n = Examples::vector_n(TRIDIAGONAL_SIZE);
 
     examine(bad_matrix.view(), vector.view());
     examine(hilbert.view(), hilbert_vec.view());
+    examine(tridiagonal.view(), vector_n.view());
     Ok(())
 }
 
@@ -38,22 +44,42 @@ fn examine(matrix: ArrayView2<f64>, vector: ArrayView1<f64>) {
         "".to_string(),
     );
 
-    let matrix_variations = VARIATIONS
-        .map(|var| variate_matrix_solve(matrix.view(), vector.view(), var))
-        .map(|variance| format!("{:.4}", variance))
-        .to_vec();
-    let matrix_variations = push_back_to_row(matrix_variations, "Matrix".to_string());
+    let matrix_variation = matrix_variations(matrix, vector, VARIATIONS.to_vec());
+    let matrix_neg_variation = matrix_variations(matrix, vector, NEGATIVE_VARIATIONS.to_vec());
 
-    let vector_variations = VARIATIONS
-        .map(|var| variate_vector_solve(matrix.view(), vector.view(), var))
-        .map(|variance| format!("{:.4}", variance))
-        .to_vec();
-    let vector_variations = push_back_to_row(vector_variations, "Vector".to_string());
+    let vector_variation = vector_variations(matrix, vector, VARIATIONS.to_vec());
+    let vector_neg_variation = vector_variations(matrix, vector, NEGATIVE_VARIATIONS.to_vec());
 
     table.add_row(headers);
-    table.add_row(matrix_variations);
-    table.add_row(vector_variations);
+    table.add_row(matrix_variation);
+    table.add_row(matrix_neg_variation);
+    table.add_row(vector_variation);
+    table.add_row(vector_neg_variation);
     table.printstd()
+}
+
+fn vector_variations(matrix: ArrayView2<f64>, vector: ArrayView1<f64>, variations: Vec<f64>) -> Row {
+    let vector_variations = variations.iter()
+        .map(|&var| variate_vector_solve(matrix.view(), vector.view(), var))
+        .map(|variance| format!("{:.4}", variance))
+        .collect();
+
+    let row_name = if variations[0].is_sign_negative() { "Vector (-)" } else {"Vector (+)"};
+
+    let vector_variations = push_back_to_row(vector_variations, row_name.to_string());
+    vector_variations
+}
+
+fn matrix_variations(matrix: ArrayView2<f64>, vector: ArrayView1<f64>, variations: Vec<f64>) -> Row {
+    let matrix_variations = variations.iter()
+        .map(|&var| variate_matrix_solve(matrix.view(), vector.view(), var))
+        .map(|variance| format!("{:.4}", variance))
+        .collect();
+
+    let row_name = if variations[0].is_sign_negative() { "Matrix (-)" } else {"Matrix (+)"};
+
+    let matrix_variations = push_back_to_row(matrix_variations, row_name.to_string());
+    matrix_variations
 }
 
 fn push_back_to_row(mut vec: Vec<String>, text: String) -> Row {
