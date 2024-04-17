90c2f15d84c0271277ffbcadd19a129f2eda3da6
Piletskii-Oleg <piletskii.oleg@gmail.com>
2024-04-17 15:39:00 +0300

    fix rotation_matrix and create rotation_table functions

--- matrices/src/lib.rs
+++ matrices/src/lib.rs
@@ -4,15 +4,11 @@ pub use examples::*;
 
 mod examples;
 
-pub fn generate_matrix<F: Fn(usize, usize) -> f64>(n: usize, generator: F) -> Array2<f64> {
+pub fn generate_matrix<T, F: Fn(usize, usize) -> T>(n: usize, generator: F) -> Array2<T> {
     let matrix = (0..n)
         .map(|row| (0..n).map(|column| generator(row, column)).collect())
-        .collect::<Vec<Vec<f64>>>();
+        .collect::<Vec<Vec<T>>>();
 
     let dimensions = (n, n);
-    Array2::from_shape_vec(
-        dimensions,
-        matrix.into_iter().flatten().collect::<Vec<f64>>(),
-    )
-    .unwrap()
+    Array2::from_shape_vec(dimensions, matrix.into_iter().flatten().collect::<Vec<T>>()).unwrap()
 }
--- matrix_solver/src/lib.rs
+++ matrix_solver/src/lib.rs
@@ -1,17 +1,38 @@
-use ndarray::Array2;
+use ndarray::{Array2, ArrayView1, ArrayView2};
 
-pub fn rotation_matrix((i, j): (usize, usize), n: usize, phi: f64) -> Array2<f64> {
-    matrices::generate_matrix(n, |row, column| {
+pub fn rotation_matrix((i, j): (usize, usize), vector: ArrayView1<f64>) -> Array2<f64> {
+    let denominator = 1.0 / (vector[i] * vector[i] + vector[j] * vector[j]).sqrt();
+    let cos = vector[i] / denominator;
+    let sin = -vector[j] / denominator;
+
+    matrices::generate_matrix(vector.len(), |row, column| {
         if (row == i && column == i) || (row == j && column == j) {
-            phi.cos()
+            cos
         } else if row == i && column == j {
-            -phi.sin()
+            -sin
         } else if row == j && column == i {
-            phi.sin()
+            sin
         } else if row == column {
             1.0
         } else {
             0.0
         }
     })
+}
+
+pub fn rotation_table(matrix: ArrayView2<f64>) -> Option<Array2<Array2<f64>>> {
+    if matrix.ncols() != matrix.nrows() {
+        return None;
+    }
+
+    let n = matrix.nrows();
+    let matrices_matrix = matrices::generate_matrix(n, |row, column| {
+        if row < column {
+            rotation_matrix((row, column), matrix.column(row)) // is this correct?
+        } else {
+            Array2::default((n, n))
+        }
+    });
+
+    Some(matrices_matrix)
 }
