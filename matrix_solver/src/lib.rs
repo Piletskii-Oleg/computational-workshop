10984b778b6146a1fed1558691ae896d61dbb262
Piletskii-Oleg <piletskii.oleg@gmail.com>
2024-04-22 16:48:01 +0300

    implement qr decomposition

--- matrix_solver/src/lib.rs
+++ matrix_solver/src/lib.rs
@@ -1,7 +1,8 @@
-use ndarray::{Array2, ArrayView1, ArrayView2};
+use ndarray::{Array1, Array2, ArrayView1, ArrayView2};
+use ndarray_linalg::Solve;
 
 pub fn rotation_matrix((i, j): (usize, usize), vector: ArrayView1<f64>) -> Array2<f64> {
-    let denominator = 1.0 / (vector[i] * vector[i] + vector[j] * vector[j]).sqrt();
+    let denominator = (vector[i] * vector[i] + vector[j] * vector[j]).sqrt();
     let cos = vector[i] / denominator;
     let sin = -vector[j] / denominator;
 
@@ -20,19 +21,71 @@ pub fn rotation_matrix((i, j): (usize, usize), vector: ArrayView1<f64>) -> Array
     })
 }
 
-pub fn rotation_table(matrix: ArrayView2<f64>) -> Option<Array2<Array2<f64>>> {
-    if matrix.ncols() != matrix.nrows() {
-        return None;
+pub fn rotation_matrices(matrix: ArrayView2<f64>) -> Array2<Array2<f64>> {
+    let n = matrix.nrows();
+    matrices::generate_matrix(n, |row, column| {
+        rotation_matrix((row, column), matrix.column(row))
+    })
+}
+
+pub fn r_matrix(matrix: ArrayView2<f64>) -> Array2<f64> {
+    let n = matrix.nrows();
+
+    (0..n).fold(matrix.to_owned(), |acc, i| {
+        (i + 1..n).fold(acc, |acc2, j| {
+            let rotation_matrix = rotation_matrix((i, j), matrix.column(i));
+            rotation_matrix.dot(&acc2)
+        })
+    })
+}
+
+pub fn q_matrix(matrix: ArrayView2<f64>) -> Array2<f64> {
+    let n = matrix.nrows();
+    let unit = Array2::eye(n);
+
+    (0..n).fold(unit, |acc, i| {
+        (i + 1..n).fold(acc, |acc2, j| {
+            let rotation_matrix = rotation_matrix((i, j), matrix.column(i));
+            let transposed = rotation_matrix.t();
+            transposed.dot(&acc2)
+        })
+    })
+}
+
+pub fn dissolve(matrix: ArrayView2<f64>, vector: ArrayView1<f64>) -> (Array2<f64>, Array1<f64>) {
+    let n = matrix.nrows();
+
+    let rotation_matrices = rotation_matrices(matrix);
+    let mut r = matrix.to_owned();
+    let mut y = vector.to_owned();
+    let mut q = Array2::eye(n);
+    for i in 0..n {
+        for j in i + 1..n {
+            let rotation_matrix = rotation_matrices[(i, j)].view();
+
+            r = rotation_matrix.dot(&r);
+            y = rotation_matrix.dot(&y);
+            q = q.dot(&rotation_matrix.t());
+        }
     }
 
-    let n = matrix.nrows();
-    let matrices_matrix = matrices::generate_matrix(n, |row, column| {
-        if row < column {
-            rotation_matrix((row, column), matrix.column(row)) // is this correct?
-        } else {
-            Array2::default((n, n))
-        }
-    });
+    (r, y)
+}
 
-    Some(matrices_matrix)
+pub fn r_vector(matrix: ArrayView2<f64>, vector: ArrayView1<f64>) -> Array1<f64> {
+    let n = matrix.nrows();
+    (0..n - 1).fold(vector.to_owned(), |acc, i| {
+        (i + 1..n).fold(acc, |acc2, j| {
+            let rotation_matrix = rotation_matrix((i, j), matrix.column(i));
+            rotation_matrix.dot(&acc2)
+        })
+    })
+}
+
+pub fn solve_qr(matrix: ArrayView2<f64>, vector: ArrayView1<f64>) -> Option<Array1<f64>> {
+    let r = r_matrix(matrix);
+    let y = r_vector(matrix, vector);
+
+    println!("r: {r:.3?}\ny: {y:.3?}");
+    Some(r.solve((&y).into()).unwrap())
 }
--- matrix_solver/src/main.rs
+++ matrix_solver/src/main.rs
@@ -1,3 +1,16 @@
+use ndarray::{array, Array2};
+use ndarray_linalg::Solve;
+
+use matrix_solver::solve_qr;
+
 fn main() {
-    println!("Hello, world!");
+    let matrix = array![[2.0, 2.0, -1.0], [3.0, 4.0, 2.0], [5.0, 6.4, 1.1]];
+    let unit: Array2<f64> = Array2::eye(matrix.nrows());
+    let matrix_by_unit = matrix.clone().dot(&unit);
+    assert_eq!(matrix.clone(), matrix_by_unit);
+    let vector = array![4.0, 5.0, -5.4];
+    let x_correct = matrix.solve(&vector);
+    let x_qr = solve_qr(matrix.view(), vector.view()).unwrap();
+
+    println!("{x_correct:.3?}\n{x_qr:.3?}")
 }
