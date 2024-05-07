7f7739225858c9af6daa69aa487942ff8a2000b9
Piletskii-Oleg <piletskii.oleg@gmail.com>
2024-05-07 16:06:50 +0300

    implement heat_equation

--- heat_equation/src/lib.rs
+++ heat_equation/src/lib.rs
@@ -1,1 +1,119 @@
+use ndarray::{Array1, Array2, ShapeBuilder};
+use ndarray_linalg::{Scalar, Solve};
 
+/// u_t(x, t) = kappa * u_xx(x, t) + f(x, t)
+///
+/// k > 0, 0 < x < a, 0 < t <= T
+///
+/// u(x, 0) = mu(x), 0 <= x <= a
+///
+/// u(0, t) = mu1(t), u(a, t) = mu2(t), 0 <= t <= T
+pub struct Equation {
+    pub kappa: f64,
+    pub f: fn((f64, f64)) -> f64,
+    pub a: f64,
+    pub T: f64,
+    pub mu: fn(f64) -> f64,
+    pub mu1: fn(f64) -> f64,
+    pub mu2: fn(f64) -> f64,
+}
+
+pub struct Solution {
+    pub x_grid: Array1<f64>,
+    pub t_grid: Array1<f64>,
+    pub approximate_solution: Array2<f64>,
+}
+
+pub fn solve_explicit(equation: &Equation, n: usize, m: usize) -> Solution {
+    let x_grid = Array1::linspace(0.0, equation.a, n + 1);
+    let t_grid = Array1::linspace(0.0, equation.T, m + 1);
+
+    let h = equation.a / n as f64;
+    let tau = equation.T / m as f64;
+
+    let mut solution = Array2::zeros((m + 1, n + 1).f());
+
+    for i in 0..n + 1 {
+        solution[(0, i)] = (equation.mu)(x_grid[i]);
+    }
+
+    for j in 1..m + 1 {
+        solution[(j, 0)] = (equation.mu1)(t_grid[j]);
+        solution[(j, n)] = (equation.mu2)(t_grid[j]);
+    }
+
+    for j in 1..m + 1 {
+        for i in 1..n {
+            let c = tau * equation.kappa / (h * h);
+
+            solution[(j, i)] = c * solution[(j - 1, i - 1)]
+                + (1.0 - 2.0 * c) * solution[(j - 1, i)]
+                + c * solution[(j - 1, i + 1)]
+                + tau * (equation.f)((x_grid[i], t_grid[j - 1]));
+        }
+    }
+
+    Solution {
+        x_grid,
+        t_grid,
+        approximate_solution: solution,
+    }
+}
+
+pub fn solve_implicit(equation: &Equation, n: usize, m: usize) -> Solution {
+    let x_grid = Array1::linspace(0.0, equation.a, n + 1);
+    let t_grid = Array1::linspace(0.0, equation.T, m + 1);
+
+    let h = equation.a / n as f64;
+    let tau = equation.T / m as f64;
+
+    let mut solution = Array2::zeros((m + 1, n + 1).f());
+
+    for i in 0..n + 1 {
+        solution[(0, i)] = (equation.mu)(x_grid[i]);
+    }
+
+    for j in 1..m + 1 {
+        solution[(j, 0)] = (equation.mu1)(t_grid[j]);
+        solution[(j, n)] = (equation.mu2)(t_grid[j]);
+    }
+
+    let a_matrix = local_matrix(equation, n, m);
+
+    for j in 1..m + 1 {
+        let mut vector = Array1::zeros((n + 1).f());
+        vector[0] = (equation.mu1)(t_grid[j]);
+        for i in 1..n {
+            vector[i] = solution[(j - 1, i)] + tau * (equation.f)((x_grid[i], t_grid[j]));
+        }
+        vector[n] = (equation.mu2)(t_grid[j]);
+
+        let approx_vector = a_matrix.solve(&vector).unwrap();
+        for i in 0..x_grid.len() {
+            solution[(j, i)] = approx_vector[i];
+        }
+    }
+
+    Solution {
+        x_grid,
+        t_grid,
+        approximate_solution: solution,
+    }
+}
+
+fn local_matrix(equation: &Equation, n: usize, m: usize) -> Array2<f64> {
+    let h = equation.a / n as f64;
+    let tau = equation.T / m as f64;
+
+    let mut matrix = Array2::zeros((n + 1, n + 1).f());
+
+    matrix[(0, 0)] = 1.0;
+    for i in 1..n {
+        matrix[(i, i - 1)] = -tau * equation.kappa / (h.square());
+        matrix[(i, i)] = 2.0 * tau * equation.kappa / (h.square()) + 1.0;
+        matrix[(i, i + 1)] = -tau * equation.kappa / (h.square());
+    }
+    matrix[(n, n)] = 1.0;
+
+    matrix
+}
--- heat_equation/src/main.rs
+++ heat_equation/src/main.rs
@@ -1,3 +1,81 @@
+use std::f64::consts::PI;
+
+use ndarray_linalg::Scalar;
+use plotters::prelude::*;
+
+use heat_equation::{solve_explicit, solve_implicit, Equation, Solution};
+
 fn main() {
-    println!("Hello, world!");
+    let equation = Equation {
+        kappa: 1.0 / PI.square(),
+        f: |(x, t)| 0.0,
+        a: 1.0,
+        T: 1.0,
+        mu: |t| (PI * t).sin() / PI.square(),
+        mu1: |_| 0.0,
+        mu2: |_| 0.0,
+    };
+
+    let explicit = solve_explicit(&equation, 15, 30);
+    let implicit = solve_implicit(&equation, 20, 30);
+    draw_solution("explicit_solution", explicit);
+    draw_solution("implicit_solution", implicit);
+}
+
+fn draw_solution(file: &str, solution: Solution) {
+    println!("{}", solution.approximate_solution);
+    let path = format!("{file}.png");
+    let root = BitMapBackend::new(&path, (640, 480)).into_drawing_area();
+
+    root.fill(&WHITE).unwrap();
+
+    let mut chart = ChartBuilder::on(&root)
+        .margin(20)
+        .caption(file, ("sans-serif", 40))
+        .build_cartesian_3d(0.0..1.0, 0.0..0.3, 0.0..1.0)
+        .unwrap();
+
+    chart.with_projection(|mut pb| {
+        pb.pitch = 0.4;
+        pb.yaw = 2.4;
+        pb.scale = 0.9;
+        pb.into_matrix()
+    });
+
+    chart.configure_axes().draw().unwrap();
+
+    let n = solution.approximate_solution.ncols();
+    let m = solution.approximate_solution.nrows();
+    let mut data = vec![];
+
+    for i in (0..n) {
+        let mut row = vec![];
+        for j in (0..m) {
+            row.push((
+                solution.x_grid[i],
+                solution.approximate_solution[(j, i)],
+                solution.t_grid[j],
+            ));
+        }
+        data.push(row);
+    }
+
+    chart
+        .draw_series(
+            (0..n - 1)
+                .map(|x| std::iter::repeat(x).zip(0..m - 1))
+                .flatten()
+                .map(|(x, z)| {
+                    Polygon::new(
+                        vec![
+                            data[x][z],
+                            data[x + 1][z],
+                            data[x + 1][z + 1],
+                            data[x][z + 1],
+                        ],
+                        &BLUE.mix(0.3),
+                    )
+                }),
+        )
+        .unwrap();
 }
